2f43a2a360d4b73b980596a0ebd02e138f51358f
SohaibZafar1986 <sohaib@orenda.systems>
2024-07-26 22:55:32 +0500

    Add styling for login form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  reactiveLoginForm = new FormGroup({
+   email : new FormControl('', [Validators.required, Validators.email]),
+   password : new FormControl('',[Validators.required]),
+  });
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  get email(){
+    return this.reactiveLoginForm.get("email");
+  }
+
+  get password(){
+    return this.reactiveLoginForm.get("password");
   }
 
 }
