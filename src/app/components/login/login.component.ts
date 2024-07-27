50c2f1133ae54e6b5e19c0f2b240475c100412f1
Sohaib-Zafar <sohaibzafar71@gmail.com>
2024-07-27 22:05:05 +0500

    Integrate login form with firebase

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { HotToastService } from '@ngneat/hot-toast';
 
 @Component({
   selector: 'app-login',
@@ -9,21 +12,41 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   reactiveLoginForm = new FormGroup({
-   email : new FormControl('', [Validators.required, Validators.email]),
-   password : new FormControl('',[Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
   });
 
-  constructor() { }
+  constructor(
+    protected _authService: AuthenticationService,
+    private router: Router,
+    private toast: HotToastService) { }
 
   ngOnInit(): void {
   }
 
-  get email(){
+  get email() {
     return this.reactiveLoginForm.get("email");
   }
 
-  get password(){
+  get password() {
     return this.reactiveLoginForm.get("password");
+  }
+
+  Submit() {
+    if (!this.reactiveLoginForm.valid) {
+      return;
+    }
+
+    const { email, password } = this.reactiveLoginForm.value;
+    this._authService.login(email ? email : '', password ? password : '').pipe(
+      this.toast.observe({
+        success: "Logged in successfully",
+        loading: "Logging in...",
+        error: "There was an error",
+      })
+    ).subscribe(() => {
+      this.router.navigate(['/home']);
+    });
   }
 
 }
