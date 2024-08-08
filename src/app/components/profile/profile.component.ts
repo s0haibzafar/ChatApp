c7f9fbd7b21643d236762c323aa53d3809709183
Sohaib-Zafar <sohaibzafar71@gmail.com>
2024-08-08 21:17:26 +0500

    create profile component

---
+++ src/app/components/profile/profile.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { HotToastService } from '@ngneat/hot-toast';
+import { concatMap} from 'rxjs';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { ImageUploadService } from 'src/app/services/image-upload.service';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+
+  user$ = this.authService.currentUser$;
+
+  profileForm = new FormGroup({
+    uid: new FormControl(''),
+    displayName: new FormControl(''),
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    phone: new FormControl(''),
+    address: new FormControl(''),
+  });
+
+  constructor(
+    private authService: AuthenticationService,
+    private imageUploadService: ImageUploadService,
+    private toast: HotToastService,
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  saveProfile() {
+
+  }
+
+  uploadFile(event: any, user: any) {
+    console.log("user --. ", user);
+    const { uid } = user;
+    this.imageUploadService
+      .uploadImage(event.target.files[0], `images/profile/${uid}`)
+      .pipe(
+        this.toast.observe({
+          loading: 'Uploading profile image...',
+          success: 'Image uploaded successfully',
+          error: 'There was an error in uploading the image',
+        }),
+        concatMap((photoURL) =>
+          this.authService.updateProfileData({
+            photoURL,
+          })
+        )
+      )
+      .subscribe();
+  }
+
+
+}
