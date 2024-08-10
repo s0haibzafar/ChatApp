ad5e0ab2ca61abe86556962e660c4e6f7aca3e55
Sohaib-Zafar <sohaibzafar71@gmail.com>
2024-08-10 19:25:52 +0500

    Add feature to update user data

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { HotToastService } from '@ngneat/hot-toast';
 import { concatMap} from 'rxjs';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 import { ImageUploadService } from 'src/app/services/image-upload.service';
+import { UserService } from 'src/app/services/user.service';
+import { ProfileUser } from 'src/app/models/user/user';
 
+@UntilDestroy()
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -12,7 +16,7 @@ import { ImageUploadService } from 'src/app/services/image-upload.service';
 })
 export class ProfileComponent implements OnInit {
 
-  user$ = this.authService.currentUser$;
+  user$ = this.userService.currentUserProfile$;
 
   profileForm = new FormGroup({
     uid: new FormControl(''),
@@ -27,14 +31,18 @@ export class ProfileComponent implements OnInit {
     private authService: AuthenticationService,
     private imageUploadService: ImageUploadService,
     private toast: HotToastService,
+    private userService:UserService
   ) { }
 
   ngOnInit(): void {
+    this.userService.currentUserProfile$.pipe(
+      untilDestroyed(this)
+    ).subscribe((user)=>{
+      this.profileForm.patchValue({ ...user });
+    });
   }
 
-  saveProfile() {
 
-  }
 
   uploadFile(event: any, user: any) {
     console.log("user --. ", user);
@@ -48,12 +56,24 @@ export class ProfileComponent implements OnInit {
           error: 'There was an error in uploading the image',
         }),
         concatMap((photoURL) =>
-          this.authService.updateProfileData({
-            photoURL,
+          this.userService.updateUser({
+            uid : user.uid,
+            photoURL
           })
         )
       )
       .subscribe();
+  }
+
+  saveProfile() {
+    const profileData = this.profileForm.value;
+    this.userService.updateUser( profileData as ProfileUser).pipe(
+      this.toast.observe({
+        loading: "Updating data...",
+        success: "Data has been updated",
+        error: "There was error in updating data"
+      })
+    ).subscribe();
   }
 
 
