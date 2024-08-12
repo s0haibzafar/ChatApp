e7c0f3abf8cb535d82a8864add53225f409dbe1c
Sohaib-Zafar <sohaibzafar71@gmail.com>
2024-08-12 23:11:01 +0500

    Add chat route under auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -31,7 +31,7 @@ const routes: Routes = [
   {
     path: 'chat',
     component: ChatComponent,
-    // ...canActivate(redirectToLogIn)
+    ...canActivate(redirectToLogIn)
   },
   {
     path: 'login',
