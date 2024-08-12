d71aa832150437cdf1c7144e04ddb7dc4cee0b75
Sohaib-Zafar <sohaibzafar71@gmail.com>
2024-08-12 23:12:54 +0500

    WIP chat layout

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { combineLatest, map, startWith } from 'rxjs';
+import { ProfileUser } from 'src/app/models/user/user';
+import { ChatService } from 'src/app/services/chat.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-chat',
@@ -7,9 +12,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
 
-  constructor() { }
+  searchControl = new FormControl('');
+
+  user$ = this.userService.currentUserProfile$;
+  // users$ = this.userService.allUsers$;
+  users$ = combineLatest([
+    this.userService.allUsers$,
+    this.user$,
+    this.searchControl.valueChanges.pipe(startWith(''))
+  ]).pipe(
+    map(([users, user, searchString]) =>
+      users.filter(u =>
+        u?.displayName?.toLowerCase().includes((searchString ?? '').toLowerCase())
+        && u.uid !== user?.uid
+      )
+    )
+  );
+
+  constructor(private userService: UserService,
+    private chatService: ChatService
+  ) { 
+    
+  }
 
   ngOnInit(): void {
+  }
+
+  createChat(otherUser: ProfileUser) {
+    this.chatService.createChat(otherUser).subscribe();
   }
 
 }
